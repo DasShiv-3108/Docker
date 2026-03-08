f6bb785c2faaa256dac13cc9670084b85dc92d8f
DasShiv-3108 <dasshiv3108@gmail.com>
2026-03-08 18:03:22 +0000

    Delete: all project

--- django/database/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id   INT PRIMARY KEY,
-    name VARCHAR(50),
-    age  INT
-);
-INSERT INTO users VALUES (1, 'Ali', 22);
-INSERT INTO users VALUES (2, 'Sara', 25);
-INSERT INTO users VALUES (3, 'Usman', 28);
